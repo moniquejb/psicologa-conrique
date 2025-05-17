d4ea31417e7a07cd1ce253f71fa1871162f44ba9
Monique Blignaut <monique.blignaut@touraxis.com>
2025-05-17 12:51:59 +0200

    First commit

---
+++ nuxt.config.ts
@@ -0,0 +1,32 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
+
+export default defineNuxtConfig({
+  compatibilityDate: '2025-05-15',
+  devtools: { enabled: true },
+
+  modules: [
+    '@nuxt/content',
+    '@nuxt/eslint',
+    '@nuxt/fonts',
+    '@nuxt/icon',
+    '@nuxt/image',
+    '@nuxt/scripts',
+    '@nuxt/ui',
+    '@nuxthq/studio'
+  ],
+
+  css: ['~/assets/css/tailwind.css'],
+
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
+
+  content: {
+    preview: {
+      api: 'https://api.nuxt.studio'
+    }
+  }
+})
