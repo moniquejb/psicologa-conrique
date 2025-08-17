90e93ece250e7961ff511362aa2960dc9af99577
moniquejb <54107967+moniquejb@users.noreply.github.com>
2025-08-17 07:33:15 +0000

    Deploying to gh-pages from @ moniquejb/psicologa-conrique@03093251407c6e083a28bab28c2ab7ece9e970c5 🚀

---
+++ _nuxt/Cp7DdnFS.js
@@ -0,0 +1,1 @@
+import{_ as s,d as a,i as r,P as t}from"./q6K6qKGP.js";const n={};function o(e,c){return r(),a("table",null,[t(e.$slots,"default")])}const _=Object.assign(s(n,[["render",o]]),{__name:"ProseTable"});export{_ as default};
